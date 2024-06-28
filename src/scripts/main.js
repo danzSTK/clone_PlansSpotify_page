1377a1118125c15203765d7b6afd72599e6d44e3
Daniel_Félix <carlosdanielfelixfarias@gmail.com>
2024-06-28 14:35:17 -0300

    adicionando função de dropar o menu de opção do profile

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttonDropPlans = document.getElementById('drop-plans');
     const menuPlans = document.querySelector('#menu_plans');
+    const buttonDropMenuProfile = document.getElementById('drop-menu-profile')
+    const menuProfile = document.querySelector('#menu-profile');
+    const iconeAnimateProfile = document.querySelector('#icone-profile-animate')
+
 
     buttonDropPlans.addEventListener('mouseenter', function(){
         menuPlans.classList.add('header__mp--open')
@@ -17,4 +21,18 @@ document.addEventListener('DOMContentLoaded', function(){
     menuPlans.addEventListener('mouseleave', function(){
         menuPlans.classList.remove('header__mp--open')
     })
+
+    buttonDropMenuProfile.onclick = () =>{
+        dropMenuProfile()
+    }
+
+    function dropMenuProfile(){
+        if(menuProfile.classList.contains('menu__profile--open')){
+            menuProfile.classList.remove('menu__profile--open')
+            iconeAnimateProfile.classList.remove('profile__angle--open')
+        } else{
+            menuProfile.classList.add('menu__profile--open')
+            iconeAnimateProfile.classList.add('profile__angle--open')
+        }
+    }
 })

1dee96b8be55ac594b2537e127d80e2e8ced1d70
Daniel_Félix <carlosdanielfelixfarias@gmail.com>
2024-07-03 15:38:10 -0300

    conclução da header e responsividade do menu mobile

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,7 +4,12 @@ document.addEventListener('DOMContentLoaded', function(){
     const buttonDropMenuProfile = document.getElementById('drop-menu-profile')
     const menuProfile = document.querySelector('#menu-profile');
     const iconeAnimateProfile = document.querySelector('#icone-profile-animate')
-
+    const buttonDropMenuMobile = document.getElementById('drop-menu-mobile')
+    const closeIcone = document.querySelectorAll('.hm__drop__icon')
+    const backgroundMenuMobile = document.querySelector('.hm__background ')
+    const menuMobile = document.querySelector('.mb__menu')
+    const itensMenuMobile = document.querySelectorAll('.mb__menu__link')
+    const menuLogo = document.querySelector('.mb__menu__home')
 
     buttonDropPlans.addEventListener('mouseenter', function(){
         menuPlans.classList.add('header__mp--open')
@@ -26,6 +31,10 @@ document.addEventListener('DOMContentLoaded', function(){
         dropMenuProfile()
     }
 
+    buttonDropMenuMobile.onclick = () => {
+        dropMenuMobile()
+    }
+
     function dropMenuProfile(){
         if(menuProfile.classList.contains('menu__profile--open')){
             menuProfile.classList.remove('menu__profile--open')
@@ -35,4 +44,41 @@ document.addEventListener('DOMContentLoaded', function(){
             iconeAnimateProfile.classList.add('profile__angle--open')
         }
     }
+
+    function dropMenuMobile(){
+        
+        
+        for( icone of closeIcone){
+            if(icone.classList.contains('hm__drop__icon--open')){
+                icone.classList.remove('hm__drop__icon--open')
+            } else{
+                icone.classList.add('hm__drop__icon--open')
+            }
+        }
+        
+        for(links of itensMenuMobile){
+            
+            if(links.classList.contains('mb__menu__link--open')){
+                links.classList.remove('mb__menu__link--open')
+            }else if(!links.classList.contains('mb__menu__link--open')){
+                links.classList.add('mb__menu__link--open')
+            }
+        }
+
+        if(menuMobile.classList.contains('mb__menu--open')){
+            menuMobile.classList.remove('mb__menu--open')
+        }else{
+            menuMobile.classList.add('mb__menu--open')
+        }
+
+        if(!menuLogo.classList.contains('mb__menu__home--open')){
+            console.log(true)
+        }else if(menuLogo.classList.contains('mb__menu__home--open')){
+            console.log(false)
+        }
+    }
 })
+
+function teste(){
+    
+}
